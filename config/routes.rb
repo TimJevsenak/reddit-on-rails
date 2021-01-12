359029cbb8edd8bfdfa5cfa9b184d26a6158f5fe
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-01-12 13:07:27 +0100

    Added communities with scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :communities
   devise_for :users, controllers: {
     confirmations: 'confirmations'
   }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :dashboard, only: [:index]
   resources :home, only: [:index]
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_094813) do
+ActiveRecord::Schema.define(version: 2021_01_12_113312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "communities", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.text "description"
+    t.string "icon"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_communities_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -34,4 +44,5 @@ ActiveRecord::Schema.define(version: 2021_01_05_094813) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "communities", "users"
 end
