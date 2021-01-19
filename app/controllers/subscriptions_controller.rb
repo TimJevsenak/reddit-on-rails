184a41290c4145c263c01f7e902fba399256949b
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-01-19 16:06:13 +0100

    User can also unsubscribe

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,4 +1,6 @@
 class SubscriptionsController < ApplicationController
+    before_action :authenticate_user!
+
     def create
         @subscription = Subscription.new(subscription_params)
         @subscription.user_id = current_user.id
@@ -6,6 +8,15 @@ class SubscriptionsController < ApplicationController
         @subscription.save
             
         redirect_to community_path(@subscription.community_id)
+    end
+
+    def destroy
+        @community_id = params[:id]
+        @user_id = current_user.id
+        @subscription = Subscription.where(:community_id => @community_id, :user_id => @user_id)
+
+        @subscription.first.destroy
+        redirect_to community_path(params[:id])
     end
 
     def subscription_params
