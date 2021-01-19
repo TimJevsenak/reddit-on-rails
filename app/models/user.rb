6f62561cd1f74cfe3d097914d76fabc9441c3262
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-01-19 13:08:31 +0100

    Adeed username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  validates_uniqueness_of :username
+
   has_many :communities
 end
