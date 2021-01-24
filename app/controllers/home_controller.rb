1028059a1b06ec28b4c86c663018c03c52c4252c
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-01-24 19:14:11 +0100

    orderd top communities by number of members

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @communities = Community.all.limit(5)
+    @communities = Community.all
+    @communities_sidebar = Community.left_joins(:subscriptions).group(:id).order('COUNT(subscriptions.id) DESC').limit(5)
   end
 end
