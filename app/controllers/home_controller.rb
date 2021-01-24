43c10745f7ba93cf067e08b551173fc38886f4c8
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-01-24 20:29:26 +0100

    Display posts on home page by date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
   def index
+    @posts ||= []
     @communities = Community.all
+    @communities.each do |community|
+      @is_subscribed = user_signed_in? ? Subscription.where(community_id: community.id, user_id: current_user.id).any? : false
+      if @is_subscribed
+        @posts += community.posts
+      end
+      @posts = @posts.sort_by(&:"#{:created_at}").reverse
+    end
+
     @communities_sidebar = Community.left_joins(:subscriptions).group(:id).order('COUNT(subscriptions.id) DESC').limit(5)
   end
 end
