079be6237e2b9adab91b61738d3bc33126a7442b
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-01-24 18:08:05 +0100

    display username and users avatar on header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :get_current_user
     
     protected
     
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:account_update, keys: [:username, :bio, :avatar, :avatar_cache])
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
+
+    def get_current_user
+        @header_user = current_user
     end
 end
