c82189035df0da1924132162c99c6122b294dc68
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-01-24 20:57:30 +0100

    User can still see posts when not logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,17 @@
 class HomeController < ApplicationController
   def index
-    @posts ||= []
+    @posts_subbed ||= []
     @communities = Community.all
     @communities.each do |community|
       @is_subscribed = user_signed_in? ? Subscription.where(community_id: community.id, user_id: current_user.id).any? : false
       if @is_subscribed
-        @posts += community.posts
+        @posts_subbed += community.posts
       end
-      @posts = @posts.sort_by(&:"#{:created_at}").reverse
+      @posts_subbed = @posts_subbed.sort_by(&:"#{:created_at}").reverse
     end
+
+    @posts= Post.all
+    @posts= Post.order(created_at: :desc)
 
     @communities_sidebar = Community.left_joins(:subscriptions).group(:id).order('COUNT(subscriptions.id) DESC').limit(5)
   end
