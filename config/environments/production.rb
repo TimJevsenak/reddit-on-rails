08a203c09689a9ffe19083438b865b0fd8b4d810
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-01-26 13:25:39 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,15 +65,17 @@ Rails.application.configure do
   #Devise
   config.action_mailer.default_url_options = { host: 'read-it-tim.herokuapp.com'}
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => ENV['GMAIL_USERNAME'],
     :password => ENV['GMAIL_PASSWORD'],
-    :domain => 'gmail.com',
+    :domain => 'read-it-tim.herokuapp.com',
     :address => 'smtp.gmail.com',
     :port => 587,
-    :authentication => :login,
+    :authentication => 'login',
     :enable_starttls_auto => true
   }
 
