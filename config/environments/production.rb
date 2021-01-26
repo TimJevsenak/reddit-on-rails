3fecf9bb0900d64bd0b836817da375ada5e671ec
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-01-26 13:05:07 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,12 +68,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'read-it-tim.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
+    :user_name => ENV['GMAIL_USERNAME'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :domain => 'https://read-it-tim.herokuapp.com/',
+    :address => 'smtp.gmail.com',
     :port => 587,
-    :authentication => :plain,
+    :authentication => :login,
     :enable_starttls_auto => true
   }
 
