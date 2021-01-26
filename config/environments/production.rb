b1c2627999d662cdb94e3ab5dbff969b412b4bc6
TimJevsenak <62609595+TimJevsenak@users.noreply.github.com>
2021-01-26 20:29:34 +0100

    minor bug fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['GMAIL_USERNAME'],
     :password => ENV['GMAIL_PASSWORD'],
-    :domain => 'read-it-tim.herokuapp.com',
+    :domain => 'https://read-it-tim.herokuapp.com',
     :address => 'smtp.gmail.com',
     :port => 587,
     :authentication => 'plain',
